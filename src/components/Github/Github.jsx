45acb60884e5e24b62bf522cdf35c00dc1f6c771
Raghu128 <119107622+Raghu128@users.noreply.github.com>
2024-06-03 09:19:37 +0530

    Change in the github file for fetching the api

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link, NavLink} from 'react-router-dom'
 import './Github.css'
 
 export default function Github() {
+    const [data, setData] = useState([]);
+    const resp = fetch('https://api.github.com/users/hiteshchoudhary');
+
+    resp.then(res => res.json())
+    .then(data => setData(data));
     return (
         <main>
-            <h1>Github</h1>
+            <h1>Github = User : {data.name} and Followers are : {data.followers}</h1>
         </main>
     );
 }
